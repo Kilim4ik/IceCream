2ac55babbeea6592b7cdd2513e0d9a098cf145b4
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2025-04-10 19:02:59 +0200

    add Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./index.css";
 import Header from "./ui/sections/header/Header.jsx";
+import Hero from "./ui/sections/hero/Hero.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
--- src/ui/components/buttons/button/Buttons.jsx
+++ src/ui/components/buttons/button/Buttons.jsx
@@ -1,24 +1,34 @@
 import styles from "./button.module.css";
+
 export default function Button({
   variant = "primary",
   arrow = false,
   type = "button",
+  link = "",
   children,
 }) {
   return (
-    <button type={type} className={styles[variant]}>
-      {children}
-      {arrow && (
-        <svg
-          aria-label="arrow"
-          width={7}
-          height={10}
-          viewBox=" 0 0 7 10"
-          className={styles.arrow}
-        >
-          <use href="/icons/sprite.svg#arrow" />
-        </svg>
+    <>
+      {link ? (
+        <a className={styles[variant]} href={link}>
+          {children}
+        </a>
+      ) : (
+        <button type={type} className={styles[variant]}>
+          {children}
+          {arrow && (
+            <svg
+              aria-label="arrow"
+              width={7}
+              height={10}
+              viewBox="0 0 7 10"
+              className={styles.arrow}
+            >
+              <use href="/icons/sprite.svg#arrow" />
+            </svg>
+          )}
+        </button>
       )}
-    </button>
+    </>
   );
 }
